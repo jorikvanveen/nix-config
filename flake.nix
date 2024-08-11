9df266656036e49fce473dce9c4cf2a582511735
Jorik <vanveenjorik@protonmail.com>
2024-08-11 22:49:35 +0200

    Add home-manager input

--- flake.nix
+++ flake.nix
@@ -4,18 +4,20 @@
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
     nixpkgs_stable.url = "github:nixos/nixpkgs?ref=nixos-24.05";
+    home-manager.url = "github:nix-community/home-manager";
+    home-manager.inputs.nixpkgs.follows = "nixpkgs_stable";
   };
 
-  outputs = { self, nixpkgs, nixpkgs_stable } @ inputs:
+  outputs = { self, nixpkgs, nixpkgs_stable, ... } @ inputs:
   let
-    unstable = nixpkgs.legacyPackages.x86_64-linux; 
+    unstable = nixpkgs.legacyPackages.x86_64-linux;
     pkgs = nixpkgs_stable.legacyPackages.x86_64-linux;
   in
   {
     nixosConfigurations.nixos-laptop = nixpkgs.lib.nixosSystem {
       specialArgs = { inherit inputs; };
       modules = [
-	./configuration.nix
+        ./configuration.nix
       ];
     };
   };
