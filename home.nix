d28a4c2faa6b3ff7384854533a64b0ea1d6c9a26
Jorik <vanveenjorik@protonmail.com>
2024-08-11 23:16:19 +0200

    Test home manager with git

--- home.nix
+++ home.nix
@@ -5,5 +5,11 @@
     stateVersion = "24.05";
   };
 
+  programs.git = {
+    enable = true;
+    userEmail = "test@test.org";
+    userName = "test";
+  };
+
   programs.home-manager.enable = true;
 }
