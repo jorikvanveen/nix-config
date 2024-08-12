892f3e7fc2660e2267a98f454f3cedafbe80df09
Jorik <vanveenjorik@protonmail.com>
2024-08-12 11:26:05 +0200

    Add NUR

--- flake.nix
+++ flake.nix
@@ -6,9 +6,10 @@
     nixpkgs_stable.url = "github:nixos/nixpkgs?ref=nixos-24.05";
     home-manager.url = "github:nix-community/home-manager";
     home-manager.inputs.nixpkgs.follows = "nixpkgs_stable";
+    nur.url = "github:nix-community/NUR";
   };
 
-  outputs = { self, nixpkgs, nixpkgs_stable, home-manager } @ inputs:
+  outputs = { self, nixpkgs, nixpkgs_stable, home-manager, nur } @ inputs:
   let
     unstable = nixpkgs.legacyPackages.x86_64-linux;
     pkgs = nixpkgs_stable.legacyPackages.x86_64-linux;
