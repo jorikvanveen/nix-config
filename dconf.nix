e48f377dc5abaf78cbcbc101127e9ec5fc867076
Jorik <vanveenjorik@protonmail.com>
2024-08-12 20:39:17 +0200

    Undo thing that broke my settings daemon

--- dconf.nix
+++ dconf.nix
@@ -127,9 +127,9 @@ with lib.hm.gvariant;
     minimize = [];
   };
 
-  "org/gnome/settings-daemon/plugins/media-keys" = {
-    custom-keybindings=["org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0/" ];
-  };
+  #"org/gnome/settings-daemon/plugins/media-keys" = {
+  #  custom-keybindings=["org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0/" ];
+  #};
 
   "org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0" = {
     binding = "<Shift><Super>Return";
