f39bcefec2af729a8af049cf619aaae5e1d711da
Jorik <vanveenjorik@protonmail.com>
2024-08-12 20:35:50 +0200

    Fix open term keybind

--- dconf.nix
+++ dconf.nix
@@ -127,6 +127,10 @@ with lib.hm.gvariant;
     minimize = [];
   };
 
+  "org/gnome/settings-daemon/plugins/media-keys" = {
+    custom-keybindings=["org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0/" ];
+  };
+
   "org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0" = {
     binding = "<Shift><Super>Return";
     command = "alacritty";
