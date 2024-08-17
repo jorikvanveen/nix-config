7413a9d57d64319d24aef2576f681a9dacbe4ccf
Jorik <vanveenjorik@protonmail.com>
2024-08-17 14:26:08 +0200

    Add ny and do fontconfig

--- configuration.nix
+++ configuration.nix
@@ -197,7 +197,16 @@
     fira-code
     inputs.apple-fonts.packages.x86_64-linux.sf-mono-nerd
     inputs.apple-fonts.packages.x86_64-linux.sf-pro
+    inputs.apple-fonts.packages.x86_64-linux.ny
   ];
+
+  fonts.fontconfig = {
+    defaultFonts = {
+      serif = [  "New York" ];
+      sansSerif = [ "SF Pro" ];
+      monospace = [ "SFMono Nerd Font" ];
+    };
+  };
 
   services.syncthing = {
     enable = true;
