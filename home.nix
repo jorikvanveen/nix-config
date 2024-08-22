39f090ed301a97b584bd87549a44372483d17c90
Jorik <vanveenjorik@protonmail.com>
2024-08-22 12:06:40 +0200

    Add node workaround for zed

--- home.nix
+++ home.nix
@@ -12,6 +12,20 @@
       target = home + "/.config/zed";
       recursive = true;
     };
+
+
+  };
+
+  # NodeJS workaround for Zed https://github.com/zed-industries/zed/issues/12631
+  xdg.dataFile = let
+    nodeVersion = "node-v22.5.1-linux-x64";
+    nodePackage = pkgs.nodejs_18;
+  in {
+    # Adding different directories one by one, as Zed wants to write to the "./cache" dir
+    "zed/node/${nodeVersion}/bin".source = "${nodePackage}/bin";
+    "zed/node/${nodeVersion}/include".source = "${nodePackage}/include";
+    "zed/node/${nodeVersion}/lib".source = "${nodePackage}/lib";
+    "zed/node/${nodeVersion}/share".source = "${nodePackage}/share";
   };
 
   programs.git = import program-config/git.nix;
