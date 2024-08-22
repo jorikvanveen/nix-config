91dfb1d8ff643e535d3ec2c85fd891b5080331a4
Jorik <vanveenjorik@protonmail.com>
2024-08-22 13:23:49 +0200

    Add toggle warning keybind and switch zed to node 22

--- home.nix
+++ home.nix
@@ -19,7 +19,7 @@
   # NodeJS workaround for Zed https://github.com/zed-industries/zed/issues/12631
   xdg.dataFile = let
     nodeVersion = "node-v22.5.1-linux-x64";
-    nodePackage = pkgs.nodejs_18;
+    nodePackage = pkgs.nodejs_22;
   in {
     # Adding different directories one by one, as Zed wants to write to the "./cache" dir
     "zed/node/${nodeVersion}/bin".source = "${nodePackage}/bin";
@@ -47,7 +47,7 @@
     enable = true;
     configFile.source = program-config/nushell/config.nu;
   };
-  
+
   programs.direnv = {
     enable = true;
     enableBashIntegration = true;
