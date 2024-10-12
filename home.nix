331114fbd652d7b1e657ab497962b0c7d9d3273b
Jorik <vanveenjorik@protonmail.com>
2024-10-12 23:47:58 +0200

    Change lsd alisases

--- home.nix
+++ home.nix
@@ -57,8 +57,21 @@
     # configFile.source = program-config/nushell/config.nu;
     extraConfig = ''
       alias core-ls = ls
-      def --wrapped ls [...rest] { lsd ...$rest --json --icon always --icon-theme fancy --color always | from json | get 0.content | select display type date | update date {|row| $row.date | into datetime} }
-      def --wrapped lsdata [...rest] { lsd ...$rest --json --icon always --icon-theme fancy --color always | from json | get 0.content | update date {|row| $row.date | into datetime} }
+      def --wrapped ls [...rest] {
+        lsd ...$rest --json --icon always --icon-theme fancy --color always
+        | from json
+        | get 0.content
+        | select display type size date
+        | update date {|row| $row.date | into datetime}
+        | update size {|row| $row.size | into filesize}
+      }
+      def --wrapped lsdata [...rest] {
+        lsd ...$rest --json --icon always --color never
+        | from json
+        | get 0.content
+        | update date {|row| $row.date | into datetime}
+        | update size {|row| $row.size | into filesize}
+      }
     '';
   };
 
