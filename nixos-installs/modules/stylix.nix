7d6e7acb92cd78c9ac861eee800c002b1eaa5a12
Jorik <vanveenjorik@protonmail.com>
2024-10-15 13:02:31 +0200

    Change cursor and set dark mode

--- nixos-installs/modules/stylix.nix
+++ nixos-installs/modules/stylix.nix
@@ -7,4 +7,7 @@
   stylix.enable = true;
   stylix.base16Scheme = "${pkgs.base16-schemes}/share/themes/catppuccin-mocha.yaml";
   stylix.image = ./.bg.jpg;
+  stylix.cursor.package = pkgs.bibata-cursors;
+  stylix.cursor.name = "Bibata-Modern-Classic";
+  stylix.polarity = "dark";
 }
