8760d01c2bd921bda725e429c7f8a89338be06ec
Jorik <vanveenjorik@protonmail.com>
2024-10-15 13:05:07 +0200

    Fix alacritty cursor

--- nixos-installs/modules/fix-alacritty-cursor.nix
+++ nixos-installs/modules/fix-alacritty-cursor.nix
@@ -1,4 +1,4 @@
 {
   # Fixes bug in alacritty + gnome where cursor would be buggy and sometimes disappear.
-  environment.sessionVariables.XCURSOR_THEME = "Adwaita";
+  environment.sessionVariables.XCURSOR_THEME = "Bibata-Modern-Classic";
 }
