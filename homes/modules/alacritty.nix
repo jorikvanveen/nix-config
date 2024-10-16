791e42ba400ba77f7e1ed03f395296ffa2ef32e1
Jorik <vanveenjorik@protonmail.com>
2024-10-16 11:13:06 +0200

    Give alacritty some padding

--- homes/modules/alacritty.nix
+++ homes/modules/alacritty.nix
@@ -2,6 +2,6 @@
   programs.alacritty.enable = true;
   programs.alacritty.settings = {
     window.decorations = "None";
-    window.padding = { x = 1; y = 1; };
+    window.padding = { x = 5; y = 5; };
   };
 }
