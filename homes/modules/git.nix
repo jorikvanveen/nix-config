745a1df732306c6f1900a48b689248c825dd0a2e
Jorik <vanveenjorik@protonmail.com>
2024-10-16 13:52:13 +0200

    Configure git merge

--- homes/modules/git.nix
+++ homes/modules/git.nix
@@ -3,5 +3,8 @@
     enable = true;
     userEmail = "vanveenjorik@protonmail.com";
     userName = "Jorik";
+    extraConfig = {
+      pull.rebase = false; # Merge instead of rebase
+    };
   };
 }
