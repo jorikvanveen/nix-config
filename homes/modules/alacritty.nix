92a2fd8f264cdba7e2c14070ae5bb30146560723
Jorik <vanveenjorik@protonmail.com>
2024-10-16 11:10:21 +0200

    Make alacritty more pretty

--- homes/modules/alacritty.nix
+++ homes/modules/alacritty.nix
@@ -1,3 +1,8 @@
-{
+{ lib, ... }: {
   programs.alacritty.enable = true;
+  programs.alacritty.settings = {
+    window.decorations = "None";
+    window.opacity = lib.mkForce 0.7;
+    window.blur = true;
+  };
 }
