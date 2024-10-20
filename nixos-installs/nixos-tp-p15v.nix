3b03465fe850d156291450a5a0dbb4b3320da9c1
Jorik <vanveenjorik@protonmail.com>
2024-10-20 22:28:17 +0200

    Add arm emulation

--- nixos-installs/nixos-tp-p15v.nix
+++ nixos-installs/nixos-tp-p15v.nix
@@ -23,6 +23,9 @@
 
   networking.hostName = "nixos-laptop"; # Define your hostname.
 
+
+  boot.binfmt.emulatedSystems = [ "aarch64-linux" ];
+
   # This value determines the NixOS release from which the default
   # settings for stateful data, like file locations and database versions
   # on your system were taken. It‘s perfectly fine and recommended to leave
