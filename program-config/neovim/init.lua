7d9ab34f339dc61ded352b45d71aeb6074c180dc
Jorik <vanveenjorik@protonmail.com>
2024-10-22 01:21:33 +0200

    Configure coq

--- program-config/neovim/init.lua
+++ program-config/neovim/init.lua
@@ -86,7 +86,7 @@ require'telescope'.setup {
 -- COQ: Completion provider
 -- Requests completions from LSP and displays them to you in the editor
 local coq = require("coq")
-vim.cmd("COQnow")
+vim.cmd("COQnow --shut-up")
 
 -- LSPCONFIG
 local lsps = {
@@ -120,36 +120,6 @@ vim.lsp.handlers["textDocument/hover"] = vim.lsp.with(vim.lsp.handlers.hover, {
   border = "rounded",
 })
 
-
-
--- -- NVIM-CMP
--- local cmp = require'cmp'
--- 
--- cmp.setup({
---   snippet = {
---     expand = function(args)
---       require('luasnip').lsp_expand(args.body)
---     end,
---   },
---   window = {
---     -- completion = cmp.config.window.bordered(),
---     -- documentation = cmp.config.window.bordered(),
---   },
---   mapping = cmp.mapping.preset.insert({
---     ['<C-b>'] = cmp.mapping.scroll_docs(-4),
---     ['<C-f>'] = cmp.mapping.scroll_docs(4),
---     ['<C-Space>'] = cmp.mapping.complete(),
---     ['<C-e>'] = cmp.mapping.abort(),
---     ['<CR>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
---   }),
---   sources = cmp.config.sources({
---     { name = 'nvim_lsp' },
---     { name = 'luasnip' }, -- For luasnip users.
---   }, {
---     { name = 'buffer' },
---   })
--- })
-
 -- HARPOON
 vim.keymap.set("n", "<leader>ah", function() require("harpoon.mark").add_file() end)
 vim.keymap.set("n", "<leader><leader>", function() require("harpoon.ui").toggle_quick_menu() end)
@@ -163,39 +133,3 @@ vim.keymap.set("n", "<leader>7", function() require("harpoon.ui").nav_file(7) en
 vim.keymap.set("n", "<leader>8", function() require("harpoon.ui").nav_file(8) end)
 vim.keymap.set("n", "<leader>9", function() require("harpoon.ui").nav_file(9) end)
 vim.keymap.set("n", "<leader>0", function() require("harpoon.ui").nav_file(10) end)
--- To use git you need to install the plugin petertriho/cmp-git and uncomment lines below
--- Set configuration for specific filetype.
---[[ cmp.setup.filetype('gitcommit', {
-  sources = cmp.config.sources({
-    { name = 'git' },
-  }, {
-    { name = 'buffer' },
-  })
-)
-equire("cmp_git").setup() ]]-- 
-
--- Use buffer source for `/` and `?` (if you enabled `native_menu`, this won't work anymore).
-cmp.setup.cmdline({ '/', '?' }, {
-  mapping = cmp.mapping.preset.cmdline(),
-  sources = {
-    { name = 'buffer' }
-  }
-})
-
--- Use cmdline & path source for ':' (if you enabled `native_menu`, this won't work anymore).
-cmp.setup.cmdline(':', {
-  mapping = cmp.mapping.preset.cmdline(),
-  sources = cmp.config.sources({
-    { name = 'path' }
-  }, {
-    { name = 'cmdline' }
-  }),
-  matching = { disallow_symbol_nonprefix_matching = false }
-})
-
--- Set up lspconfig.
---local capabilities = require('cmp_nvim_lsp').default_capabilities()
----- Replace <YOUR_LSP_SERVER> with each lsp server you've enabled.
---require('lspconfig')['<YOUR_LSP_SERVER>'].setup {
---  capabilities = capabilities
---}
