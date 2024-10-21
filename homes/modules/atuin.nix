3b88e5342bb87ede7057a6a7a645660475f33186
Jorik <vanveenjorik@protonmail.com>
2024-10-21 12:49:28 +0200

    Enable atuin sync

--- homes/modules/atuin.nix
+++ homes/modules/atuin.nix
@@ -2,5 +2,11 @@
   programs.atuin = {
     enable = true;
     enableNushellIntegration = true;
+    settings = {
+      auto_sync = true;
+      sync_frequency = "5m";
+      sync_address = "https://atuin.jorik-dev.com";
+      search_mode = "prefix";
+    };
   };
 }
