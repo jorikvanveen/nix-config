1c1b4b061b2a004abe724a4f7e5464694af198f4
Jorik <vanveenjorik@protonmail.com>
2024-11-02 23:11:11 +0100

    Use kde specialization

--- nixos-installs/modules/gamescope-specialisation.nix
+++ nixos-installs/modules/gamescope-specialisation.nix
@@ -1,29 +1,14 @@
-{ pkgs, ... }: {
+{ lib, pkgs, ... }: {
   specialisation.gamescope.configuration = {
+    imports = [
+      ./kde.nix
+    ];
     system.nixos.tags = [ "gamescope" ];
 
-    services.xserver.desktopManager.gnome.enable = false;
-    services.xserver.displayManager.gdm.enable = false;
+    services.xserver.desktopManager.gnome.enable = lib.mkForce false;
+    services.xserver.displayManager.gdm.enable = lib.mkForce false;
 
-    boot.kernelParams = [ "video=HDMI-A-1:d" ]; # Disable second display
     #boot.kernelPackages = pkgs.linuxPackages; # (this is the default) some amdgpu issues on 6.10
-
-    programs = {
-      gamescope = {
-        enable = true;
-        capSysNice = true;
-      };
-      steam = {
-        enable = true;
-        gamescopeSession.enable = true;
-      };
-    };
     hardware.xone.enable = true; # support for the xbox controller USB dongle
-    services.getty.autologinUser = "main";
-    environment = {
-      loginShellInit = ''
-        [[ "$(tty)" = "/dev/tty1" ]] && ./gs.sh
-      '';
-    };
   };
 }
