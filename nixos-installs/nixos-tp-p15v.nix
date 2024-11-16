25166f20d9aa5fd5b2f01098c27529a1a6d508f7
Jorik <vanveenjorik@protonmail.com>
2024-11-16 20:24:01 +0100

    use gsconnect

--- nixos-installs/nixos-tp-p15v.nix
+++ nixos-installs/nixos-tp-p15v.nix
@@ -42,4 +42,5 @@
   # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
   system.stateVersion = "24.05"; # Did you read the comment?
   programs.kdeconnect.enable = true;
+  programs.kdeconnect.package = lib.mkForce pkgs.gnomeExtensions.gsconnect;
 }
