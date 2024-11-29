afc18109994e876164a90e6564b17acddf154aac
Jorik <vanveenjorik@protonmail.com>
2024-11-29 13:43:20 +0100

    Fix zurg hopefully

--- nixos-installs/modules/services/zurg.nix
+++ nixos-installs/modules/services/zurg.nix
@@ -5,6 +5,11 @@
       "9999:9999"
     ];
 
+    environment = {
+      PUID = "1000";
+      PGID = "1000";
+    };
+
     volumes = [
       "/home/main/data/zurg:/app/data"
       "/home/main/.config/zurg/config.yml:/app/config.yml"
