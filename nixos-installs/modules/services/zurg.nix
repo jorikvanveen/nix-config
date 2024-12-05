aa72417045cf0453aeb27f45a7fe626779ed6a3e
Jorik <vanveenjorik@protonmail.com>
2024-12-05 23:28:16 +0100

    Switch to torbox

--- nixos-installs/modules/services/zurg.nix
+++ nixos-installs/modules/services/zurg.nix
@@ -1,20 +1,20 @@
 {
-  virtualisation.oci-containers.containers.zurg = {
-    image = "ghcr.io/debridmediamanager/zurg-testing:latest";
-    ports = [
-      "9999:9999"
-    ];
+  # virtualisation.oci-containers.containers.zurg = {
+  #   image = "ghcr.io/debridmediamanager/zurg-testing:latest";
+  #   ports = [
+  #     "9999:9999"
+  #   ];
 
-    environment = {
-      PUID = "1000";
-      PGID = "1000";
-    };
+  #   environment = {
+  #     PUID = "1000";
+  #     PGID = "1000";
+  #   };
 
-    volumes = [
-      "/home/main/data/zurg:/app/data"
-      "/home/main/.config/zurg/config.yml:/app/config.yml"
-    ];
-  };
+  #   volumes = [
+  #     "/home/main/data/zurg:/app/data"
+  #     "/home/main/.config/zurg/config.yml:/app/config.yml"
+  #   ];
+  # };
 
   virtualisation.oci-containers.containers.rclone = {
     image = "rclone/rclone:latest";
@@ -23,12 +23,12 @@
       PGID = "1000";
     };
     volumes = [
-      "/mnt/zurg:/data:rshared"
+      "/mnt/torbox-library:/data:rshared"
       "/home/main/.config/zurg/rclone.conf:/config/rclone/rclone.conf"
     ];
     extraOptions = ["--privileged" "--network=host" ];
 
-    dependsOn = [ "zurg" ];
+    # dependsOn = [ "zurg" ];
     cmd = [ "mount" "zurg:" "/data" "--allow-other" "--allow-non-empty" "--dir-cache-time" "10s" "--vfs-cache-mode" "full" ];
   };
 }
