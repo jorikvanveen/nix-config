9bb8b81118655411e4e812ce5fdb805cce78dce8
Jorik <vanveenjorik@protonmail.com>
2024-12-05 23:57:59 +0100

    Fix webdav mount

--- nixos-installs/modules/services/torbox-rclone.nix
+++ nixos-installs/modules/services/torbox-rclone.nix
@@ -2,6 +2,6 @@
   environment.systemPackages = [ pkgs.rclone ];
   systemd.services.torbox-rclone = {
     serviceConfig.User = "main";
-    script = "rclone mount Torbox: /mnt/torbox-library/ --vfs-cache-mode full";
+    script = "rclone mount Torbox: /mnt/torbox-webdav --vfs-cache-mode full";
   };
 }
