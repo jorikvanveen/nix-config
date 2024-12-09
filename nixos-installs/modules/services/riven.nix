131a2dd0cebaac21a38d492dbc18ac8524a41e72
Jorik <vanveenjorik@protonmail.com>
2024-12-09 20:38:22 +0100

    Mount webdav as readonly

--- nixos-installs/modules/services/riven.nix
+++ nixos-installs/modules/services/riven.nix
@@ -31,7 +31,7 @@
       };
       volumes = [
         "/home/main/data/riven:/riven/data"
-        "/mnt/torbox-webdav:/torbox-webdav"
+        "/mnt/torbox-webdav:/mnt/torbox-webdav:ro"
         "/mnt/riven-library:/mnt/riven-library"
       ];
       dependsOn = [ "riven-postgres" ];
