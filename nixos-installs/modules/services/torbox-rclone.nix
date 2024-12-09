8fb99ad4cc1bc76dc288226a836a587974669650
Jorik <vanveenjorik@protonmail.com>
2024-12-09 20:52:32 +0100

    idfk

--- nixos-installs/modules/services/torbox-rclone.nix
+++ nixos-installs/modules/services/torbox-rclone.nix
@@ -1,5 +1,6 @@
 { pkgs, ... }: {
   environment.systemPackages = [ pkgs.rclone ];
+  programs.fuse.userAllowOther = true;
   systemd.services.torbox-rclone = {
     serviceConfig = {
       User = "main";
@@ -10,9 +11,10 @@
       DeviceAllow = "/dev/fuse";
       CapabilityBoundingSet = "CAP_SYS_ADMIN";
       AmbientCapabilities = "CAP_SYS_ADMIN";
+      ExecStop = "/biun/fusermount -u /mnt/torbox-webdav";
     };
     #serviceConfig.SupplementaryGroups = [ "fuse" ];
-    script = "${pkgs.rclone}/bin/rclone mount Torbox: /mnt/torbox-webdav --vfs-cache-mode full";
+    script = "${pkgs.rclone}/bin/rclone mount Torbox: /mnt/torbox-webdav --allow-other --vfs-cache-mode full";
     wantedBy = [ "multi-user.target" ];
     after = [ "network-online.target" ];
   };
