0690963f7956cfdf1eb7f5b641a92df2ec93209c
Jorik <vanveenjorik@protonmail.com>
2025-01-02 14:08:33 +0100

    Set ghostty keybinds

--- homes/modules/ghostty.nix
+++ homes/modules/ghostty.nix
@@ -1,3 +1,13 @@
 {
-  programs.ghostty.enable = true;
+  programs.ghostty = {
+    enable = true;
+    settings = {
+      keybind = [
+        "ctrl+t=new_tab"
+        "ctrl+,=previous_tab"
+        "ctrl+.=next_tab"
+        "ctrl+w=close_tab"
+      ];
+    };
+  };
 }
