0c5e7b2a320e14220cdfcf3487f1dd417f95ad80
Jorik <vanveenjorik@protonmail.com>
2025-01-02 19:29:54 +0100

    ghostty: fix copy paste binds

--- homes/modules/ghostty.nix
+++ homes/modules/ghostty.nix
@@ -8,8 +8,8 @@
         "ctrl+,=previous_tab"
         "ctrl+.=next_tab"
         "ctrl+q=close_surface"
-        "ctrl+shift+c=copy_from_clipboard"
-        "ctrl+shift+v=paste_from_selection"
+        "ctrl+shift+c=copy_to_clipboard"
+        "ctrl+shift+v=paste_from_clipboard"
       ];
     };
   };
