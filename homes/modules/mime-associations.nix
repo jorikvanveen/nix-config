42498d7a5b578ff89ca1dddd0089be3c91dac8ea
Jorik <vanveenjorik@protonmail.com>
2025-01-09 10:39:47 +0100

    mime-associations: set zen as default browser

--- homes/modules/mime-associations.nix
+++ homes/modules/mime-associations.nix
@@ -4,14 +4,20 @@
     defaultApplications = {
       "video/mp4" = "vlc.desktop";
       "text/calendar" = "gnome-calendar.desktop";
-      "x-scheme-handler/http" = "zen.desktop";
-      "x-scheme-handler/https" = "zen.desktop";
-      "text/html" = "zen.desktop";
-      "application/xhtml+xml" = "zen.desktop";
       "image/jpeg" = "org.gnome.Loupe.desktop";
       "image/png" = "org.gnome.Loup.desktop";
       "image/webp" = "org.gnome.Loupe.desktop";
+      "application/x-extension-xht" = "zen.desktop";
       "image/gif" = "org.gnome.Loupe.desktop";
+      "x-scheme-handler/http" = "zen.desktop";
+      "x-scheme-handler/https" = "zen.desktop";
+      "x-scheme-handler/chrome" = "zen.desktop";
+      "text/html" = "zen.desktop";
+      "application/x-extension-htm" = "zen.desktop";
+      "application/x-extension-html" = "zen.desktop";
+      "application/x-extension-shtml" = "zen.desktop";
+      "application/xhtml+xml" = "zen.desktop";
+      "application/x-extension-xhtml" = "zen.desktop";
     };
   };
 }
