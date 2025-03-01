c82d153e52d6252604ca1b865dd8550a19337bb7
Jorik <vanveenjorik@protonmail.com>
2025-03-01 13:14:21 +0100

    litellm: Use stable

--- nixos-installs/modules/services/open-webui.nix
+++ nixos-installs/modules/services/open-webui.nix
@@ -6,7 +6,7 @@
   };
 
   systemd.services.litellm-claude = let
-    py-env = (unstable.python3.withPackages
+    py-env = (pkgs-stable.python3.withPackages
       (ppkgs: with ppkgs; [ litellm ] ++ litellm.optional-dependencies.proxy ++ litellm.optional-dependencies.extra_proxy));
   in {
     enable = true;
