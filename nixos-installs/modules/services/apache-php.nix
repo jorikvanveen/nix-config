af1fef2cdb8d108a182da0817722b2f7274c5b00
Jorik <vanveenjorik@protonmail.com>
2025-03-06 20:30:07 +0100

    apache: use virtualhosts

--- nixos-installs/modules/services/apache-php.nix
+++ nixos-installs/modules/services/apache-php.nix
@@ -1,6 +1,11 @@
 { pkgs, ... }: {
   services.httpd = {
     enable = true;
+    virtualHosts = {
+      localhost = {
+        documentRoot = "/srv/apache/avdvegt/public_html"; 
+      };
+    };
     phpPackage = pkgs.php.buildEnv {
       extensions = ({ enabled, all }: enabled ++ (with all; [
           xdebug
