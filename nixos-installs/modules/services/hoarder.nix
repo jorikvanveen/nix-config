afd9f270459d99cbe325e93ffe46ec646d61f13b
Jorik <vanveenjorik@protonmail.com>
2025-03-10 17:54:39 +0100

    hoarder: fix chromium

--- nixos-installs/modules/services/hoarder.nix
+++ nixos-installs/modules/services/hoarder.nix
@@ -38,15 +38,15 @@ in {
     hoarder-browser = {
        image = "lscr.io/linuxserver/chromium:latest";
        pull = "newer";
-       cmd = [
-          "--"
-          "--no-sandbox"
-          "--disable-gpu"
-          "--disable-dev-shm-usage"
-          "--remote-debugging-address=0.0.0.0"
-          "--remote-debugging-port=9222"
-          "--hide-scrollbars"
-       ];
+       #cmd = [
+       #   "--"
+       #   "--no-sandbox"
+       #   "--disable-gpu"
+       #   "--disable-dev-shm-usage"
+       #   "--remote-debugging-address=0.0.0.0"
+       #   "--remote-debugging-port=9222"
+       #   "--hide-scrollbars"
+       #];
     };
   };
 }
