6bf2e7a3638effcd8938fb4cbeb2f2c6a751f1a1
Jorik <vanveenjorik@protonmail.com>
2025-03-16 21:23:40 +0100

    Mount qb dls via rclone

---
+++ nixos-installs/modules/sshfs.nix
@@ -0,0 +1,23 @@
+{ pkgs, ... }: {
+  environment.systemPackages = [ pkgs.rclone ];
+
+  environment.etc."sshfs-qbd.conf".text = ''
+    [jorik-dev]
+    type = sftp
+    host = j.jorik-dev.com
+    user = main
+    key_file = /home/main/.ssh/id_ed25519
+  '';
+
+  fileSystems."/mnt/qb" = {
+    device = "jorik-dev:/mnt/media/data/qbittorrent/downloads";
+    fsType = "rclone";
+    options = [
+      "nodev"
+      "nofail"
+      "allow_other"
+      "args2env"
+      "config=/etc/sshfs-qbd.conf"
+    ];
+  };
+}
