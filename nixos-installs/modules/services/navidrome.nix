2094a44dae206a8b0bdf98d026c89e9615a292ac
Jorik <vanveenjorik@protonmail.com>
2025-03-23 17:18:06 +0100

    navidrome: allow reads from qb library

--- nixos-installs/modules/services/navidrome.nix
+++ nixos-installs/modules/services/navidrome.nix
@@ -2,6 +2,10 @@
   services.navidrome = {
     enable = true;
     settings.Port = 8089;
-    settings.MusicFolder = "/mnt/media/data/qbittorrent/downloads";
+    settings.MusicFolder = "/mnt/media/music";
   };
+
+  systemd.services.navidrome.serviceConfig.BindReadOnlyPaths = [
+    "/mnt/media/data/qbittorrent/downloads"
+  ];
 }
