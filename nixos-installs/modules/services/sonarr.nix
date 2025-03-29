d924c17cdee4a70204b0fbe0702d36c321fb495d
Jorik <vanveenjorik@protonmail.com>
2025-03-29 19:14:24 +0100

    add 4k sonarr

--- nixos-installs/modules/services/sonarr.nix
+++ nixos-installs/modules/services/sonarr.nix
@@ -1,10 +1,27 @@
-{ pkgs, ... }: {
+{ pkgs, utils, ... }: {
   services.sonarr = {
     enable = true;
     #package = pkgs.sonarr;
     user = "main";
     group = "users";
     dataDir = "/home/main/data/sonarr";
+  };
+
+  systemd.services.sonarr-4k = {
+    description = "Sonarr 4k";
+    after = [ "network.target" ];
+    wantedBy = [ "multi-user.target" ];
+    serviceConfig = {
+      Type = "simple";
+      User = "main";
+      Group = "users";
+      ExecStart = utils.escapeSystemdExecArgs [
+        (pkgs.lib.getExe pkgs.sonarr)
+        "-nobrowser"
+        "-data=/home/main/data/sonarr-4k"
+      ];
+      Restart = "on-failure";
+    };
   };
 
   # Requred to use sonarr at all as of 8 jan 2025 (yikes)
