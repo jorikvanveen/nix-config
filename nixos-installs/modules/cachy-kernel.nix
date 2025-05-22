2b05c959cd45bfe86c679b8010c6c79c2c7addad
Jorik <vanveenjorik@protonmail.com>
2025-05-23 00:08:13 +0200

    Pin linux to work around amdgpu regression

--- nixos-installs/modules/cachy-kernel.nix
+++ nixos-installs/modules/cachy-kernel.nix
@@ -3,5 +3,17 @@
     inputs.chaotic.nixosModules.default
   ];
 
-  boot.kernelPackages = pkgs.linuxPackages_cachyos;
+  #boot.kernelPackages = pkgs.linuxPackages_cachyos;
+
+  # Linux 6.14.7 contains amdgpu regression
+  boot.kernelPackages = pkgs.linuxPackagesFor (pkgs.linux_6_14.override rec {
+    argsOverride = {
+      src = pkgs.fetchurl {
+        url = "mirror://kernel/linux/kernel/v6.x/linux-${version}.tar.xz";
+        sha256 = "";
+      };
+    };
+    version = "6.14.6";
+    modDirVersion = "6.14.6";
+  });
 }
