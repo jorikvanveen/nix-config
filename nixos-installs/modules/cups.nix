a413d03ef9a495f8fe9d7e9667a21e43c36d570b
Jorik <vanveenjorik@protonmail.com>
2025-06-06 12:18:42 +0200

    Add brother printer driver

--- nixos-installs/modules/cups.nix
+++ nixos-installs/modules/cups.nix
@@ -1,7 +1,7 @@
 { pkgs, ... }: {
   # Enable CUPS to print documents.
   services.printing.enable = true;
-  services.printing.drivers = [ pkgs.brlaser ];
+  services.printing.drivers = [ pkgs.brlaser pkgs.brgenml1lpr pkgs.brgenml1cupswrapper ];
   services.samba.enable = true;
   services.avahi = {
     enable = true;
