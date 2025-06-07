04b7400f5fd0df2000d36d259d9a21770144cabc
Jorik <vanveenjorik@protonmail.com>
2025-06-07 15:40:19 +0200

    Make swaylock button work in flake

--- nixos-installs/modules/niri.nix
+++ nixos-installs/modules/niri.nix
@@ -27,7 +27,7 @@ in {
   systemd.user.services.ags-bar = {
     wantedBy = [ "graphical-session.target" ];
     script = "${ags-bar}/bin/my-shell";
-    path = [ pkgs.bash pkgs.niri pkgs.ffmpeg pkgs.jack_capture pkgs.gvfs ];
+    path = [ pkgs.bash pkgs.niri pkgs.ffmpeg pkgs.jack_capture pkgs.gvfs pkgs.swaylock ];
     serviceConfig = {
       Restart = "always";
       RestartSec = "2s";
