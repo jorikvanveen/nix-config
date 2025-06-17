919516fc761b8d93632ffa1234be71b63efeb981
Jorik <vanveenjorik@protonmail.com>
2025-06-17 19:11:24 +0200

    Fix syncthing

--- homes/nixos-wsl.nix
+++ homes/nixos-wsl.nix
@@ -1,3 +1,11 @@
-{
-  imports = [ ./generic.nix ];
+{ inputs, ... }: {
+  imports = [
+    ./generic.nix
+    ./modules/main-user.nix
+    inputs.nixos-wsl.nixosModules.default
+    {
+      system.stateVersion = "25.11";
+      wsl.enable = true;
+    }
+  ];
 }
