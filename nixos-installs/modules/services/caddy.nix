cb20b1fcf2c2cd6a0168774670218385cbd60246
Jorik <vanveenjorik@protonmail.com>
2025-07-03 21:03:27 +0200

    Secure ebookdl

--- nixos-installs/modules/services/caddy.nix
+++ nixos-installs/modules/services/caddy.nix
@@ -98,6 +98,10 @@
       '';
       "ebookdl.jorik-dev.com".extraConfig = ''
         reverse_proxy http://localhost:3300
+        basic_auth {
+          # Username "Bob", password "hiccup"
+          ebookdl $apr1$wEjE/dVD$ixS1NogTa6wsff/PkpW.40
+        }
       '';
     };
   };
