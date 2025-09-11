3a96a9ccd8bdf04c9fa187acdff199e0604a1cbe
Jorik <vanveenjorik@protonmail.com>
2025-09-11 15:26:58 +0200

    add ci webhook

--- nixos-installs/modules/services/ci-webhook.nix
+++ nixos-installs/modules/services/ci-webhook.nix
@@ -1,8 +1,15 @@
-{
+{ pkgs, ... }: {
   services.webhook = {
     enable = true;
     hooks.deploy-hovyu = {
       execute-command = "/home/main/data/deploy-hovyu.sh";
+    };
+  };
+
+  systemd.services.webhook = {
+    serviceConfig = {
+      User = pkgs.lib.mkForce "main";
+      Group = pkgs.lib.mkForce "users";
     };
   };
 }
