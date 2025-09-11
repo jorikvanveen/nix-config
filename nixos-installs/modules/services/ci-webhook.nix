152f568ce563c0822b91c6e10c4594c280d69490
Jorik <vanveenjorik@protonmail.com>
2025-09-11 17:20:51 +0200

    add ci webhook

--- nixos-installs/modules/services/ci-webhook.nix
+++ nixos-installs/modules/services/ci-webhook.nix
@@ -2,7 +2,14 @@
   services.webhook = {
     enable = true;
     hooks.deploy-hovyu = {
-      execute-command = "/home/main/data/deploy-hovyu.sh";
+      execute-command = "/home/main/data/hovyu-scripts/deploy-hovyu.sh";
+      command-working-directory = "/home/main/data/hovyu-scripts";
+      pass-arguments-to-command = [
+        {
+          source = "payload";
+          name = "job_id";
+        }
+      ];
     };
   };
 
