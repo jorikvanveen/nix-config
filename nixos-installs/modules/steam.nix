cc1cd909c982f0089248a1c5c36b25bbce1e8b42
Jorik <vanveenjorik@protonmail.com>
2025-09-15 17:03:26 +0200

    add gamescope

--- nixos-installs/modules/steam.nix
+++ nixos-installs/modules/steam.nix
@@ -3,12 +3,24 @@
     enable = true;
     remotePlay.openFirewall = true;
     dedicatedServer.openFirewall = true;
+    package = pkgs.steam.override {
+      extraPkgs = pkgs': with pkgs'; [
+        xorg.libXcursor
+        xorg.libXi
+        xorg.libXinerama
+        xorg.libXScrnSaver
+        libpng
+        libpulseaudio
+        libvorbis
+        stdenv.cc.cc.lib # Provides libstdc++.so.6
+        libkrb5
+        keyutils
+        # Add other libraries as needed
+      ];
+    };
   };
 
   hardware.xone.enable = true;
 
-  environment.systemPackages  = [
-    pkgs.mangohud
-    pkgs.gamescope
-  ];
+  environment.systemPackages = [ pkgs.mangohud pkgs.gamescope ];
 }
