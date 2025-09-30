a6b29e6147c066c55e8e31e123760c555cb307ae
Jorik <vanveenjorik@protonmail.com>
2025-09-30 16:39:08 +0200

    actual-flow timer

--- nixos-installs/modules/services/actual.nix
+++ nixos-installs/modules/services/actual.nix
@@ -1,31 +1,29 @@
-{ pkgs, ... }: {
+{ pkgs, ... }:
+{
   services.actual = {
     enable = true;
     settings.port = 30313;
     settings.hostname = "0.0.0.0";
   };
 
-  systemd.user.services."actual-flow-import" = {
-    Unit = {
-      Description = "Actual Flow Import";
-    };
-    Service = {
-      Type = "oneshot";
-      WorkingDirectory = "/home/main/data/actual-flow";
-      ExecStart = "${pkgs.nodejs}/bin/npx --yes @lunchflow/actual-flow import";
+  systemd.user.timers."actual-flow-import" = {
+    wantedBy = [ "timers.target" ];
+    timerConfig = {
+      OnCalendar = "daily";
+      Persistent = true;
+      Unit = "actual-flow-import.service";
     };
   };
 
-  systemd.user.timers."actual-flow-import" = {
-    Unit = {
-      Description = "Run Actual Flow Import daily";
-    };
-    Timer = {
-      OnCalendar = "daily";
-      Persistent = true;
-    };
-    Install = {
-      WantedBy = [ "timers.target" ];
+  systemd.user.services."actual-flow-import" = {
+    script = ''
+      set -eu
+      ${pkgs.nodejs}/bin/npx --yes @lunchflow/actual-flow import
+    '';
+    serviceConfig = {
+      Type = "oneshot";
+      User = "main"; 
+      WorkingDirectory = "/home/main/data/actual-flow"; 
     };
   };
 }
