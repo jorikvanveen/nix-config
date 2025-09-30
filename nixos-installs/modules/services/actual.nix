e1d439cc9c93c6f10f93dbd4c5bdf118b2487041
Jorik <vanveenjorik@protonmail.com>
2025-09-30 16:33:37 +0200

    add timer for actual-flow

--- nixos-installs/modules/services/actual.nix
+++ nixos-installs/modules/services/actual.nix
@@ -1,7 +1,31 @@
-{
+{ pkgs, ... }: {
   services.actual = {
     enable = true;
     settings.port = 30313;
     settings.hostname = "0.0.0.0";
+  };
+
+  systemd.user.services."actual-flow-import" = {
+    Unit = {
+      Description = "Actual Flow Import";
+    };
+    Service = {
+      Type = "oneshot";
+      WorkingDirectory = "/home/main/data/actual-flow";
+      ExecStart = "${pkgs.nodejs}/bin/npx --yes @lunchflow/actual-flow import";
+    };
+  };
+
+  systemd.user.timers."actual-flow-import" = {
+    Unit = {
+      Description = "Run Actual Flow Import daily";
+    };
+    Timer = {
+      OnCalendar = "daily";
+      Persistent = true;
+    };
+    Install = {
+      WantedBy = [ "timers.target" ];
+    };
   };
 }
