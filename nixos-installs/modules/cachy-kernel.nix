1aaa8177f6973d7925bfc2ee9b73870f7219cbc5
Jorik <vanveenjorik@protonmail.com>
2025-11-13 11:01:18 +0100

    update linux kernel

--- nixos-installs/modules/cachy-kernel.nix
+++ nixos-installs/modules/cachy-kernel.nix
@@ -3,8 +3,5 @@
     inputs.chaotic.nixosModules.default
   ];
 
-  #boot.kernelPackages = pkgs.linuxPackages_cachyos;
-
-  # Linux 6.14.7 contains amdgpu regression
-  boot.kernelPackages = inputs.nixpkgs-linux6-14-6.legacyPackages.x86_64-linux.linuxPackages;
+  boot.kernelPackages = pkgs.linuxPackages_cachyos;
 }
