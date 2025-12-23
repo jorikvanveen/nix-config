36bf60f49bd27f7c7817216a319dde7006a875d7
Jorik <vanveenjorik@protonmail.com>
2025-12-23 20:21:32 +0100

    Disable prime to maybe fix steam

--- nixos-installs/modules/steam.nix
+++ nixos-installs/modules/steam.nix
@@ -3,21 +3,21 @@
     enable = true;
     remotePlay.openFirewall = true;
     dedicatedServer.openFirewall = true;
-    #package = pkgs.steam.override {
-    #  extraPkgs = pkgs': with pkgs'; [
-    #    xorg.libXcursor
-    #    xorg.libXi
-    #    xorg.libXinerama
-    #    xorg.libXScrnSaver
-    #    libpng
-    #    libpulseaudio
-    #    libvorbis
-    #    stdenv.cc.cc.lib # Provides libstdc++.so.6
-    #    libkrb5
-    #    keyutils
-    #    # Add other libraries as needed
-    #  ];
-    #};
+    package = pkgs.steam.override {
+      extraPkgs = pkgs': with pkgs'; [
+        xorg.libXcursor
+        xorg.libXi
+        xorg.libXinerama
+        xorg.libXScrnSaver
+        libpng
+        libpulseaudio
+        libvorbis
+        stdenv.cc.cc.lib # Provides libstdc++.so.6
+        libkrb5
+        keyutils
+        # Add other libraries as needed
+      ];
+    };
   };
 
   hardware.xone.enable = true;
