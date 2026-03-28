c71432ae1387e6a30d8fae63b35f6e19a16e36fb
Jorik <vanveenjorik@protonmail.com>
2026-03-28 16:25:18 +0100

    idk

--- homes/modules/nushell.nix
+++ homes/modules/nushell.nix
@@ -17,25 +17,6 @@
       ta = "tmux a";
     };
     extraConfig = ''
-      alias core-ls = ls
-
-      def --wrapped ls [...rest] {
-        lsd ...$rest --json --icon always --icon-theme fancy --color always --hyperlink always
-        | from json
-        | get 0.content
-        | select type display size date
-        | update date {|row| $row.date | into datetime}
-        | update size {|row| $row.size | into filesize}
-      }
-
-      def --wrapped lsdata [...rest] {
-        lsd ...$rest --json --icon always --color never
-        | from json
-        | get 0.content
-        | update date {|row| $row.date | into datetime}
-        | update size {|row| $row.size | into filesize}
-      }
-
       def dui [path: glob] {
         ^du --exclude-from=/home/main/shared/personal/.stignore -s $path
         | str replace -a "\t" "  "
