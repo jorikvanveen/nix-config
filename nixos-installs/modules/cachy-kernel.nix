483e0e21985e93b08940659b02096ebbbf9b26b2
Jorik <vanveenjorik@protonmail.com>
2026-04-07 12:27:57 +0200

    zenergy

--- nixos-installs/modules/cachy-kernel.nix
+++ nixos-installs/modules/cachy-kernel.nix
@@ -1,6 +1,10 @@
-{ pkgs, inputs, ... }: {
+{ pkgs, inputs, ... }: let
+  kernel = pkgs.cachyosKernels.linuxPackages-cachyos-latest-lto;
+in {
   nixpkgs.overlays = [
     inputs.cachy-flake.overlays.pinned
   ];
-  boot.kernelPackages = pkgs.cachyosKernels.linuxPackages-cachyos-latest-lto;
+  boot.kernelPackages = kernel;
+  boot.extraModulePackages = [ kernel.zenergy ];
+  boot.kernelModules = [ "zenergy" "coretemp" "k10temp" ];
 }
