eaba52bf2c5f10335a38531fdc03f20b45fb0c8a
Jorik <vanveenjorik@protonmail.com>
2026-07-12 23:32:44 +0200

    idk niri ig

--- nixos-installs/modules/niri.nix
+++ nixos-installs/modules/niri.nix
@@ -3,7 +3,7 @@ let
   ags-bar = inputs.ags-shell.packages.x86_64-linux.default;
 in {
   programs.niri.enable = true;
-  services.xserver.displayManager.gdm.enable = lib.mkDefault true;
+  services.displayManager.sddm.enable = lib.mkDefault true;
   services.gvfs.enable = true;
   services.blueman.enable = true;
   services.gnome.evolution-data-server.enable = true;
