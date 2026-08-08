be79e5633dc61c58f6f5226e56e1f841d9125458
Jorik <vanveenjorik@protonmail.com>
2026-08-08 19:58:50 +0200

    open-webui is a fuck

--- nixos-installs/modules/services/open-webui.nix
+++ nixos-installs/modules/services/open-webui.nix
@@ -1,27 +1,12 @@
 { pkgs, inputs, system, ... }: let
-  #pkgs-stable = inputs.nixpkgs-stable.legacyPackages.${system};
+  pkgs-stable = import inputs.nixpkgs-stable {
+    inherit system;
+    config.allowUnfree = true;
+  };
 in {
   services.open-webui = {
     enable = true;
     port = 8090;
-    package = pkgs.open-webui;
-  };
-
-  systemd.services.litellm-claude = let
-    py-env = (pkgs.python3.withPackages
-      (ppkgs: with ppkgs; [ litellm ] ++ litellm.optional-dependencies.proxy ++ litellm.optional-dependencies.extra_proxy));
-  in {
-    enable = true;
-    after = [ "network.target" ];
-    wantedBy = [ "multi-user.target" ];
-    description = "OpenAI-compatible proxy for claude, used by open-webui";
-    script = ''
-      export ANTHROPIC_API_KEY=$(cat $CREDENTIALS_DIRECTORY/anthropic-key);
-      ${py-env}/bin/litellm --model anthropic/claude-3-5-sonnet-20241022 --port 4001
-    '';
-    serviceConfig = {
-      LoadCredential = "anthropic-key:/run/secrets/anthropic-key";
-      Type = "simple";
-    };
+    package = pkgs-stable.open-webui; # This build is a piece of shit
   };
 }
