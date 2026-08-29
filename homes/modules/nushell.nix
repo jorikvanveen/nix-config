db4d88618f0c639144d9cdf799c701c68aa1b1f2
Jorik <vanveenjorik@protonmail.com>
2026-08-29 14:36:01 +0200

    add random-emoji utility

--- homes/modules/nushell.nix
+++ homes/modules/nushell.nix
@@ -36,6 +36,20 @@
         | reduce --fold {} {|it, acc| $acc | insert $it.key $it.val }
         | load-env
       }
+      
+      def random-emoji [] {
+        let runs = [
+          [0x1F300 0x1F320] [0x1F32D 0x1F335] [0x1F337 0x1F37C] [0x1F37E 0x1F393]
+          [0x1F3A0 0x1F3CA] [0x1F3CF 0x1F3D3] [0x1F3E0 0x1F3F0] [0x1F3F4 0x1F3F4]
+          [0x1F3F8 0x1F42C] [0x1F42E 0x1F43E] [0x1F440 0x1F440] [0x1F442 0x1F4FC]
+          [0x1F4FF 0x1F53D] [0x1F54B 0x1F54E] [0x1F644 0x1F64F] [0x1F680 0x1F6C5]
+          [0x1F6CC 0x1F6CC] [0x1F6D0 0x1F6D2] [0x1F6D5 0x1F6D7]
+        ]
+        $runs | each {|r| seq $r.0 $r.1 } | flatten | shuffle | first
+        | into binary | bytes reverse | encode hex | str trim --left --char '0'
+        | char -u $in
+      }
+
       $env.config.edit_mode = "vi"
     '';
   };
