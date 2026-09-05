0e400cc61739012fa3ad232bdb0fe537f04a28c3
Jorik <vanveenjorik@protonmail.com>
2026-09-05 16:57:21 +0200

    zed & dnsmasq fix

--- nixos-installs/modules/network.nix
+++ nixos-installs/modules/network.nix
@@ -1,4 +1,12 @@
 {
   networking.networkmanager.enable = true;
-  services.dnsmasq.enable = true;
+  services.dnsmasq = {
+    enable = true;
+    settings = {
+      listen-address = "127.0.0.1";
+      bind-interfaces = true;
+      no-resolv = true;
+      server = [ "1.1.1.1" "1.0.0.1" ];
+    };
+  };
 }
